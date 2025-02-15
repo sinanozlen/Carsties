b5baae688627c1570a51fc8c7744a19ead1be22d
sinanozlen <157057982+sinanozlen@users.noreply.github.com>
2025-02-15 13:11:44 +0300

    Pagination

---
+++ src/SearchService/Controllers/SearchContoller.cs
@@ -0,0 +1,39 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+using SearchService.Model;
+
+namespace SearchService;
+
+
+[ApiController]
+[Route("api/search")]
+public class SearchContoller : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm,
+    int pageNumber = 1, int pageSize = 4)
+    {
+        var query = DB.PagedSearch<Item>();
+
+        query.Sort(x => x.Ascending(a => a.Make));
+
+        if (!string.IsNullOrEmpty(searchTerm))
+        {
+            query.Match(Search.Full, searchTerm).SortByTextScore();
+        }
+        
+        query.PageNumber(pageNumber);
+        query.PageSize(pageSize);
+
+
+        var result = await query.ExecuteAsync();
+
+        return Ok(new 
+        {
+            results = result.Results,
+            pageCount = result.PageCount,
+            totalCount = result.TotalCount
+        });
+    }
+}
